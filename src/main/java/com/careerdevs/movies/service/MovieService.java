a4b417d7823fafbd66d4a68a8ea7c0235e843fd0
Pierre Fils <epierrefils@gmail.com>
2023-03-16 08:32:34 -0400

    Full Stack web devlopment Movie Get Single movie by Id

---
+++ src/main/java/com/careerdevs/movies/service/MovieService.java
@@ -0,0 +1,26 @@
+package com.careerdevs.movies.service;
+
+import com.careerdevs.movies.Movie;
+import com.careerdevs.movies.repository.MovieRepository;
+import org.bson.types.ObjectId;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class MovieService {
+    @Autowired
+    private MovieRepository movieRepository;
+    @GetMapping
+    public List<Movie> allMovies() {
+        return movieRepository.findAll();
+    }
+
+    public Optional<Movie> singleMovie (ObjectId id){
+        return  movieRepository.findById(id);
+
+    }
+}
